e9ce81271885ba46c0a6419bc6ff7c45578e72e9
selena.grant <selena.d.grant@gmail.com>
2018-12-05 17:03:55 -0500

    Adding modules
    Not sure if this is enough, but we're exploring!

--- lib/oz_appium.rb
+++ lib/oz_appium.rb
@@ -2,4 +2,12 @@ require "oz_appium/version"
 
 module OzAppium
   # Your code goes here...
+  require_all('./CORE/utils')
+  require_all('./CORE/world_gadgets/router')
+  require_all('./CORE/world_gadgets')
+  require_all('./CORE/world_extensions')
+  require_relative('./CORE/elements/_core_element.rb')
+  require_all('./CORE/pages')
+
+  require_all('./CORE/step_definitions') if defined?(Cucumber)
 end
